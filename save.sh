4d749c96e1695931c600bd239be84218a51f8a09
zhangxd_home <zyyy358@126.com>
2015-05-10 18:58:27 +0800

    change

--- save.sh
+++ save.sh
@@ -2,4 +2,4 @@ cd /e/Users/workspace/eclipse/sanluan_cms
 git add -A .
 git commit -m "change"
 git push
-read –n | echo “Press enter to continue...” INP
+read –n | echo Press enter to continue... INP
