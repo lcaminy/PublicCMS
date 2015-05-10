5a6ca31978ceebf8777de825a57442ce06bd63d9
zhangxd_home <zyyy358@126.com>
2015-05-10 18:57:23 +0800

    change

--- save.sh
+++ save.sh
@@ -2,7 +2,6 @@ cd /e/Users/workspace/eclipse/sanluan_cms
 git add -A .
 git commit -m "change"
 git push
-read –n | -p “Press any key to continue...” INP
+read –n | echo “Press any key to continue...” INP
 if [[ $INP != ‘ ’ ]] ;then
-    echo –n ‘\b \n’
 fi
