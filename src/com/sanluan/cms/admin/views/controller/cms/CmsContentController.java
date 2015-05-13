6ab9dc11358a0c32231c9cebad335e2dd386d159
zhangxd_home <zyyy358@126.com>
2015-05-13 22:55:00 +0800

    change

--- src/com/sanluan/cms/admin/views/controller/cms/CmsContentController.java
+++ src/com/sanluan/cms/admin/views/controller/cms/CmsContentController.java
@@ -4,23 +4,62 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.sanluan.common.base.BaseController;
+import com.sanluan.cms.entities.cms.CmsCategory;
 import com.sanluan.cms.entities.cms.CmsContent;
+import com.sanluan.cms.entities.cms.CmsModel;
+import com.sanluan.cms.logic.component.StaticComponent;
+import com.sanluan.cms.logic.service.cms.CmsCategoryService;
 import com.sanluan.cms.logic.service.cms.CmsContentService;
+import com.sanluan.cms.logic.service.cms.CmsModelService;
+import com.sanluan.common.base.BaseController;
+
 @Controller
 @RequestMapping("cmsContent")
 public class CmsContentController extends BaseController {
 	@Autowired
 	private CmsContentService service;
+	@Autowired
+	private CmsModelService modelService;
+	@Autowired
+	private CmsCategoryService categoryService;
+	@Autowired
+	private StaticComponent staticComponent;
 
 	@RequestMapping(value = { "save" + DO_SUFFIX })
-	public String save(CmsContent entity, HttpServletRequest request) {
+	public String save(CmsContent entity, HttpServletRequest request, ModelMap model) {
 		if (null != entity.getId()) {
 			service.update(entity.getId(), entity);
 		} else {
 			service.save(entity);
+		}
+		CmsModel cmsModel = modelService.getEntity(entity.getModelId());
+		CmsCategory cmsCategory = categoryService.getEntity(entity.getCategoryId());
+		if (virifyCustom("static",
+				!staticComponent.createStaticFile(cmsModel.getTemplatePath(), cmsCategory.getContentPath(), request, model),
+				model)
+				|| virifyCustom("static1", !staticComponent.createStaticFile(cmsModel.getTemplatePath1(),
+						cmsCategory.getContentPath1(), request, model), model)) {
+			return "common/ajaxError";
+		}
+		return "common/ajaxDone";
+	}
+
+	@RequestMapping(value = { "static" + DO_SUFFIX })
+	public String publish(Integer id, HttpServletRequest request, ModelMap model) {
+		CmsContent entity = service.getEntity(id);
+		if (null != entity) {
+			CmsModel cmsModel = modelService.getEntity(entity.getModelId());
+			CmsCategory cmsCategory = categoryService.getEntity(entity.getCategoryId());
+			if (virifyCustom("static",
+					!staticComponent.createStaticFile(cmsModel.getTemplatePath(), cmsCategory.getContentPath(), request, model),
+					model)
+					|| virifyCustom("static1", !staticComponent.createStaticFile(cmsModel.getTemplatePath1(),
+							cmsCategory.getContentPath1(), request, model), model)) {
+				return "common/ajaxError";
+			}
 		}
 		return "common/ajaxDone";
 	}
---
+++ src/com/sanluan/cms/logic/component/StaticComponent.java
@@ -0,0 +1,91 @@
+package com.sanluan.cms.logic.component;
+
+import static com.sanluan.common.constants.FreeMakerConstants.CONTEXT_BASE;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import com.sanluan.common.tools.FreeMarkerUtils;
+
+import freemarker.template.Configuration;
+import freemarker.template.TemplateException;
+
+/**
+ * @author zhangxd
+ * 
+ */
+@Component
+public class StaticComponent {
+	private String staticFileDirectory;
+	private String templateLoaderPath;
+
+	@Autowired
+	private FreeMarkerConfigurer freeMarkerConfigurer;
+
+	@Autowired
+	private Configuration configuration;
+
+	public boolean createStaticFile(String templatePath, String filePath, HttpServletRequest request, ModelMap model) {
+		try {
+			if (StringUtils.isNotBlank(filePath)) {
+				model = (ModelMap) model.clone();
+				model.put(CONTEXT_BASE, request.getContextPath());
+				FreeMarkerUtils.makeFileByFile(templatePath, getHtmlFilePath(request, filePath),
+						freeMarkerConfigurer.getConfiguration(), model);
+			}
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	private String getHtmlFilePath(HttpServletRequest request, String filePath) {
+		if (StringUtils.isBlank(staticFileDirectory)) {
+			staticFileDirectory = request.getSession().getServletContext().getRealPath("/static/");
+		}
+		return staticFileDirectory + filePath;
+	}
+
+	/**
+	 * @param staticFileDirectory
+	 *            the staticFileDirectory to set
+	 */
+	public void setStaticFileDirectory(String staticFileDirectory) {
+		this.staticFileDirectory = staticFileDirectory;
+	}
+
+	/**
+	 * @return the configuration
+	 */
+	public Configuration getConfiguration() {
+		if (null == configuration) {
+			try {
+				if (StringUtils.isNotBlank(templateLoaderPath)) {
+					configuration = freeMarkerConfigurer.createConfiguration();
+					configuration.setDirectoryForTemplateLoading(new File(templateLoaderPath));
+				} else {
+					configuration = freeMarkerConfigurer.getConfiguration();
+				}
+			} catch (IOException | TemplateException e) {
+			}
+		}
+		return configuration;
+	}
+
+	/**
+	 * @param templateLoaderPath
+	 *            the templateLoaderPath to set
+	 */
+	public void setTemplateLoaderPath(String templateLoaderPath) {
+		this.templateLoaderPath = templateLoaderPath;
+	}
+
+}
